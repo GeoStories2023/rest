080a404febafa9da3638ba6d75afb5042478b1cc
Janis Meister <janis.meister87@gmail.com>
2023-03-17 10:25:09 +0100

    add tour route

---
+++ routes/tour.ts
@@ -0,0 +1,36 @@
+
+import { Request, Response, Router } from 'express';
+import { Prisma } from '@prisma/client';
+import { getPrismaInstance } from '../lib/prisma';
+import { GeostoriesRequest } from '../interfaces/request';
+import { authMiddleware } from '../middleware';
+
+export const router: Router = Router();
+
+
+router.get('/:id', authMiddleware, (req: GeostoriesRequest, res: Response) => {
+  const prisma = getPrismaInstance();
+  const id = req.params.id;
+
+  prisma.tour.findUnique({
+    where: {
+      id: id
+    }
+  }).then((tour) => {
+    res.json(tour);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  });
+});
+
+router.get('/', authMiddleware, (req: GeostoriesRequest, res: Response) => {
+  const prisma = getPrismaInstance();
+
+  prisma.tour.findMany().then((tours) => {
+    res.json(tours);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  });
+});
