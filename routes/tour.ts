9b9bb3b9bf13230cbbcbf3b7fcaf5e2defcdb673
Janis Meister <janis.meister87@gmail.com>
2023-03-23 11:06:32 +0100

    Get StartedTours

--- routes/tour.ts
+++ routes/tour.ts
@@ -7,6 +7,17 @@ import { authMiddleware } from '../middleware';
 
 export const router: Router = Router();
 
+router.get('/', (req: GeostoriesRequest, res: Response) => {
+  const prisma = getPrismaInstance();
+
+  prisma.tour.findMany().then((tours) => {
+    res.json(tours);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  });
+});
+
 
 router.get('/:id', (req: GeostoriesRequest, res: Response) => {
   const prisma = getPrismaInstance();
@@ -24,16 +35,48 @@ router.get('/:id', (req: GeostoriesRequest, res: Response) => {
   });
 });
 
-router.get('/', (req: GeostoriesRequest, res: Response) => {
+router.get('/started/', (req: GeostoriesRequest, res: Response) => {
   const prisma = getPrismaInstance();
 
-  prisma.tour.findMany().then((tours) => {
-    res.json(tours);
+  prisma.startedTour.findMany({
+    where: {
+      userId: req.user?.uid
+    },
+    include: {
+      tour: true
+    }
+  }).then((startedTours) => {
+    res.json(startedTours);
   }).catch((error) => {
     console.log(error);
     res.status(500).send('Internal server error');
   });
 });
+
+router.get('/started/:id', (req: GeostoriesRequest, res: Response) => {
+  const prisma = getPrismaInstance();
+  const id = req.params.id;
+
+  prisma.startedTour.findUnique({
+    where: {
+      id: id
+    },
+    include: {
+      tour: true
+    }
+  }).then((startedTour) => {
+
+    if (startedTour?.userId !== req.user?.uid) {
+      res.status(403).send('Forbidden');
+      return;
+    }
+    res.json(startedTour);
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).send('Internal server error');
+  });
+});
+
 
 router.post('/start/', async (req: GeostoriesRequest, res: Response) => {
   const prisma = getPrismaInstance();
