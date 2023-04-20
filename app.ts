73941ce7f46679da2ed830f2d0283b966ca668d6
Janis Meister <janis.meister87@gmail.com>
2023-04-20 12:42:08 +0200

    cors to localhost:3000

--- app.ts
+++ app.ts
@@ -20,6 +20,15 @@ app.use(cors({
   origin: 'localhost:3000',
 }));
 
+// set cors headers for localhost:3000
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
