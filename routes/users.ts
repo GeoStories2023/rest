72e8f6c013534f2593e59e2b4ab3d64126dcf2fd
Janis Meister <janis.meister87@gmail.com>
2023-05-11 13:03:06 +0200

    return bad request if username is already taken

--- routes/users.ts
+++ routes/users.ts
@@ -291,9 +291,12 @@ router.put("/setUsername", (req: GeostoriesRequest, res: Response) => {
     res.json(user);
   }).catch((error: any) => {
     console.log(error);
-    console.log(JSON.parse(error.code))
-    res.status(500).json(error.code);
-    res.status(500).send('Internal server error');
+    console.log(error.code)
+    if (error.code === 'P2002') {
+      res.status(400).send('Username already taken');
+    } else {
+      res.status(500).send('Internal server error');
+    }
   });
 });
 
